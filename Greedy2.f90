97715328407cb45abbb3eee50692f15b30a4ca4f
Tommaso Mannelli <67388177+Tommaso-Mannelli-Mazzoli@users.noreply.github.com>
2020-10-16 16:20:51 +0200

    Update Greedy2.f90

--- Greedy2.f90
+++ Greedy2.f90
@@ -1,71 +1,65 @@
- !!!!!!!!!!!!!!!!!!!!!!
-!  ALGORITMO GREEDY  ! !version 2
-!!!!!!!!!!!!!!!!!!!!!!
-  
- SUBROUTINE greedy2(q,n1,n2,vec,z) ! vec: mejor permutacion encontrada por el greedy; z: su valor de la funcion objectivo        
- USE datos     
- INTEGER,INTENT(IN):: q ! q=1 si quiero eligir n1 e n2; q=0 para hacer todas las combinaciones de n1 y n2  
- INTEGER, INTENT(IN)::n1,n2 ! para q=1 n1 negocio (planta) inicial, n2 lugar inicial 
-   
- INTEGER i,j,k, r,s                      
- INTEGER tmpn(n),tmpl(n), aux(n),p(n)              
- DOUBLE PRECISION bb(n,n),v0,v1
- DOUBLE PRECISION zp 
-      
-  INTEGER, INTENT(OUT):: vec(n)     !Permutacion final: por ejemplo vec(a)=b significa que al negocio (planta) a asocio lugar b 
- DOUBLE PRECISION, INTENT(OUT):: z
-   
-  
- z=1.d30
+! =====================================================================  
+! Greedy2
+ SUBROUTINE greedy2(q,n1,n2,p_best,z_best)    
+ USE datos    
+!.. Scalar Arguments ..           
+ INTEGER, INTENT(IN) :: q, n1, n2
+!.. Local Scalars .. 
+ INTEGER :: i , j, k, r, s  , p(n)
+ DOUBLE PRECISION :: v0, v1, z_p 
+!.. Local Arrays ..                  
+ INTEGER :: f(n), l(n), aux(n), vec(n)        
+ DOUBLE PRECISION :: bb(n,n)
+!.. Outputs .. 
+ INTEGER, INTENT(OUT) :: p_best(n) ! Final permutation: vec(a)=b means assigning location b to facility a 
+ DOUBLE PRECISION, INTENT(OUT) :: z_best
  
- DO k=1,n
-  DO j=1,n
-    tmpn=0            
-    bb = b      !bb = matriz temporal lugares 
-    tmpn(1) = j !vector temporal para negocios (plantas) asignados
-    tmpl(1) = k ! vector temporal para lugares asignados 
-    IF (q==1) THEN 
-      tmpn(1) = n1 !vector temporal para negocios (plantas) asignados
-      tmpl(1) = n2 ! vector temporal para lugares asignados 
+ z_best = 1.d30
+ DO k = 1,n
+  DO j = 1,n
+    bb(:,:) = b     
+    f(:) = 0            
+    f(1) = j 
+    l(1) = k 
+    IF (q == 1) THEN 
+      f(1) = n1
+      l(1) = n2 
     ENDIF 
-  
-    bb(tmpl(1),tmpl(1)) = 1.d30     
-  
-
-    DO i=1,n-1  
-       IF (MINVAL(bb(tmpl(i),:), DIM=1) < MINVAL(bb(:,tmpl(i)),DIM=1)) THEN                       
-        tmpl(i+1) = MINLOC( bb( tmpl(i),:),DIM=1)  
+    p(f(1)) = l(1)
+    bb(l(1),l(1)) = 1.d30       
+    DO i = 1,n-1  
+       IF (MINVAL(bb(l(i),:), DIM=1) < MINVAL(bb(:,l(i)),DIM=1)) THEN                       
+        l(i+1) = MINLOC(bb(l(i),:),DIM=1)  
        ELSE
-        tmpl(i+1) = MINLOC( bb(:, tmpl(i)),DIM=1) 
+        l(i+1) = MINLOC(bb(:,l(i)),DIM=1) 
        ENDIF
-        
-      v1 = 1.d30
-      DO s=1,n       
-         IF (MINVAL(ABS(tmpn-s)).NE. 0) THEN    ! es decir, si s no es un index de tmpn (para no asignar negocios ya asignados)    
-   
-         v0 = DOT_PRODUCT(b(tmpl(i+1),tmpl(1:i)) , a(s,tmpn(1:i)))+&
-              DOT_PRODUCT(b(tmpl(1:i),tmpl(i+1)) , a(tmpn(1:i),s)) 
-          
-         IF (v0 < v1) THEN
-             v1=v0 
-             r=s    
-         ENDIF
-        ENDIF  
+      v1 = -1.d30
+      DO s = 1,n       
+         IF (MINVAL(ABS(f-s)) /=  0) THEN       
+           v0 = DOT_PRODUCT(b(l(i+1),l(1:i)) , a(s,f(1:i))) &
+                + DOT_PRODUCT(b(l(1:i),l(i+1)) , a(f(1:i),s))           
+           IF (v0 > v1) THEN
+             v1 = v0 
+             r = s    
+           ENDIF
+         ENDIF  
       ENDDO      
-      tmpn(i+1)=r        !nuevo negocio
-      bb(tmpl(i+1),tmpl(1:i+1))=1.d30    ! actualizar a para no asignar negocios ya asignados
-      bb(tmpl(1:i+1),tmpl(i+1))=1.d30
+      f(i+1) = r               
+      p(f(i + 1)) = l(i + 1)
+      bb(l(i+1),l(1:i+1))=1.d30    ! actualizar a para no asignar negocios ya asignados
+      bb(l(1:i+1),l(i+1))=1.d30
     ENDDO 
-    CALL ISORT@(aux, tmpn,n) 
-    p=tmpl(aux) !ordino      
-   
-    CALL FUNOBJ(p,zp) 
-    IF (zp < z) THEN
-       z=zp
-       vec=p
-    ENDIF   
-    IF (q==1) GOTO 40
+    CALL funobj(p , z_p)
+    IF (q==1)THEN
+      p_best = p
+      z_best = z_p
+      RETURN
+    END IF
+    IF (z_p < z_best) THEN
+      z_best = z_p
+      p_best = p
+    END IF
+      
   ENDDO
  ENDDO
- 40 ENDSUBROUTINE greedy2 
-                         
+ ENDSUBROUTINE greedy2 
