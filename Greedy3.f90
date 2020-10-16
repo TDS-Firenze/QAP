691aee358fd4fff0749326fa09645a7a3b31b93a
Tommaso Mannelli <67388177+Tommaso-Mannelli-Mazzoli@users.noreply.github.com>
2020-10-16 16:21:20 +0200

    Update Greedy3.f90

--- Greedy3.f90
+++ Greedy3.f90
@@ -1,63 +1,76 @@
-!!!!!!!!!!!!!!!!!!!!!!!
-!  ALGORITMO GREEDY  ! !version 3
-!!!!!!!!!!!!!!!!!!!!!!  
-      
+! =====================================================================  
+! Greedy3      
  
-SUBROUTINE greedy3(q,n1,n2,vec,z_best)  
+SUBROUTINE greedy3(q,n1,n2,p_best,z_best)  
 USE datos  
-INTEGER,INTENT(IN):: q ! q=1 si quiero eligir n1 e n2; q=0 para hacer todas las combinaciones de n1 y n2   
-INTEGER, INTENT(IN):: n1,n2 ! para q=1 n1 negocio (planta) inicial, n2 lugar inicial 
-INTEGER i,j,s,k  
-INTEGER tmpn(n),tmpl(n), aux(n),p(n) 
-INTEGER, INTENT(OUT):: vec(n)
-DOUBLE PRECISION aa(n,n), bb(n,n)
-DOUBLE PRECISION zp
-DOUBLE PRECISION, INTENT(OUT)::z_best   
-z_best=1.d30 
-tmpn = 0
-tmpl = 0
-DO i=1,n 
- DO j=1,n 
-   tmpn(1)=i 
-   tmpl(1)=j
-   IF (q==1)THEN
-       tmpn(1)=n1 
-       tmpl(1)=n2
-   ENDIF  
-   aa=a  !aa = matriz temporal plantas 
-   aa(tmpn(1),tmpn(1)) = -1.d30
-   bb=b   !bb = matriz temporal lugares         
-   bb(tmpl(1),tmpl(1))=1.d30 
-   DO s=1,n-1     
-     IF (sim==1) THEN
-        tmpn(s+1) = MAXLOC( aa( tmpn(s),:),DIM=1) 
-        tmpl(s+1) = MINLOC( bb( tmpl(s),:),DIM=1)
+!.. Scalar Arguments ..           
+ INTEGER, INTENT(IN):: q,n1,n2
+!.. Local Scalars .. 
+ INTEGER i,j,k,r,s , p(n)
+ DOUBLE PRECISION v0,v1 , z_p
+!.. Local Arrays ..                  
+ INTEGER f(n),l(n), aux(n),vec(n)        
+ DOUBLE PRECISION aa(n,n), bb(n,n)
+!.. Outputs .. 
+ INTEGER, INTENT(OUT):: p_best(n) ! Final permutation: vec(a)=b means assigning location b to facility a 
+ DOUBLE PRECISION, INTENT(OUT) :: z_best
+ 
+
+z_best = 1.d30 
+p = 0
+DO i = 1,n 
+ DO j = 1,n 
+   f(:) = 0
+   l(:) = 0
+   IF (q == 1)THEN
+       f(1) = n1 
+       l(1) = n2    
+   ELSE IF (q == 0) THEN
+       f(1) = i 
+       l(1) = j
+   ENDIF                 
+   p(f(1)) = l(1)
+   aa = a 
+   aa(f(1),f(1)) = -1.d30
+   bb = b           
+   bb(l(1),l(1)) = 1.d30 
+   DO s = 1,n-1     
+     IF (sim == 1) THEN
+        f(s+1) = MAXLOC( aa(f(s),:) , DIM = 1) 
+        l(s+1) = MINLOC( bb(l(s),:) , DIM = 1)
      ELSE            
-        IF (MAXVAL(aa(tmpn(s),:),DIM=1) > MAXVAL(aa(:,tmpn(s)),DIM=1)) THEN                       
-           tmpn(s+1) = MAXLOC( aa( tmpn(s),:),DIM=1)  
+        IF (MAXVAL( aa(f(s),:) , DIM = 1) > MAXVAL( aa(:,f(s)) , DIM = 1)) THEN                       
+           f(s+1) = MAXLOC( aa(f(s),:),DIM=1)  
         ELSE
-           tmpn(s+1) = MAXLOC( aa(:, tmpn(s)),DIM=1) 
+           f(s+1) = MAXLOC( aa(:,f(s)),DIM=1) 
         ENDIF
-        IF (MINVAL(bb(tmpl(s),:), DIM=1) < MINVAL(bb(:,tmpl(s)),DIM=1)) THEN                       
-           tmpl(s+1) = MINLOC( bb( tmpl(s),:),DIM=1)  
+        IF (MINVAL(bb(l(s),:), DIM=1) < MINVAL(bb(:,l(s)),DIM=1)) THEN                       
+           l(s+1) = MINLOC( bb(l(s),:),DIM=1)  
         ELSE
-           tmpl(s+1) = MINLOC( bb(:, tmpl(s)),DIM=1) 
+           l(s+1) = MINLOC( bb(:,l(s)),DIM=1) 
         ENDIF
      ENDIF
-     aa(tmpn(1:s+1),tmpn(1:s+1))=-1.d30     ! actualizo aa para no asignar negocios (plantas) ya asignados
-     bb(tmpl(1:s+1),tmpl(1:s+1))= 1.d30     ! actualizo bb para no asignar lugares ya asignados  
-   ENDDO    
-   123 FORMAT(12(I2,1x))
-   WRITE(*,123) tmpn
-   CALL ISORT@(aux, tmpn,n) 
-   WRITE(*,123) aux
-   p = tmpl(aux) !ordino 
-   CALL FUNOBJ(p,zp)
-   IF (zp < z_best) THEN
-      z_best=zp
-      vec=p
-   ENDIF 
-   IF (q==1) RETURN 
+
+     p(f(s + 1)) = l(s + 1)
+     aa(f(1:s+1), f(s+1)) = -1.d30    
+     aa(f(s+1), f(1:s+1)) = -1.d30         
+     bb(l(s+1), l(1:s+1)) =  1.d30    
+     bb(l(1:s+1), l(s+1)) =  1.d30    
+     ENDDO    
+     CALL funobj(p , z_p)
+
+     IF (q == 1) THEN
+       p_best = p
+       z_best = z_p
+       RETURN
+     END IF
+     IF (z_p < z_best) THEN
+       z_best = z_p
+       p_best = p
+     END IF
+          
  ENDDO 
-ENDDO     
+ENDDO   
+
+ 
 ENDSUBROUTINE greedy3
